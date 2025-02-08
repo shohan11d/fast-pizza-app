644fd162bea1ab04b1f185e8ce7dbfcd8a699301
shohan11d <shohan11d@gmail.com>
2025-02-09 00:52:51 +0600

    project restarted

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,52 @@
-import { createBrowserRouter } from 'react-router';
-import { RouterProvider } from 'react-router-dom';
+// import { createBrowserRouter } from 'react-router';
+// import { RouterProvider } from 'react-router-dom';
 
-import Home from './ui/Home';
-import Menu, { loader as menuLoader } from './features/menu/Menu';
-import Cart from './features/cart/Cart';
-import AppLayout from './ui/AppLayout';
-import Error from './ui/Error';
-import CreateOrder, {
-  action as createOrderAction,
-} from './features/order/CreateOrder';
-import Order, { loader as orderLoader } from './features/order/Order';
+// import Home from './ui/Home';
+// import Menu, { loader as menuLoader } from './features/menu/Menu';
+// import Cart from './features/cart/Cart';
+// import AppLayout from './ui/AppLayout';
+// import Error from './ui/Error';
+// import CreateOrder, {
+//   action as createOrderAction,
+// } from './features/order/CreateOrder';
+// import Order, { loader as orderLoader } from './features/order/Order';
 
-const router = createBrowserRouter([
-  {
-    element: <AppLayout />,
-    errorElement: <Error />,
-    children: [
-      { path: '/', element: <Home /> },
-      {
-        path: 'menu',
-        element: <Menu />,
-        loader: menuLoader,
-        errorElement: <Error />,
-      },
-      {
-        path: 'cart',
-        element: <Cart />,
-      },
-      {
-        path: 'order/new',
-        element: <CreateOrder />,
-        action: createOrderAction,
-      },
-      {
-        path: 'order/:orderId',
-        element: <Order />,
-        loader: orderLoader,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     element: <AppLayout />,
+//     errorElement: <Error />,
+//     children: [
+//       { path: '/', element: <Home /> },
+//       {
+//         path: 'menu',
+//         element: <Menu />,
+//         loader: menuLoader,
+//         errorElement: <Error />,
+//       },
+//       {
+//         path: 'cart',
+//         element: <Cart />,
+//       },
+//       {
+//         path: 'order/new',
+//         element: <CreateOrder />,
+//         action: createOrderAction,
+//       },
+//       {
+//         path: 'order/:orderId',
+//         element: <Order />,
+//         loader: orderLoader,
+//       },
+//     ],
+//   },
+// ]);
+
+// function App() {
+//   return <RouterProvider router={router} />;
+// }
 
 function App() {
-  return <RouterProvider router={router} />;
+  return <div>Hello World</div>;
 }
 
 export default App;
